1e5738ccd2fc30a8b57422d02b274479540fd270
Dmitry Akentyev <TogusaRusso@users.noreply.github.com>
2017-03-31 22:18:51 +0300

    Create outline.js

---
+++ outline.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           Outline reply lines
+// @version        0.0.1
+// @namespace      https://tweetdeck.twitter.com/
+// @auther         Togusa
+// @description    Outline reply lines
+// @include        https://web.tweetdeck.com/*
+// @include        https://tweetdeck.twitter.com/*
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @grant          none
+// ==/UserScript==
+
+
+(function($) {
+    setInterval(() => {$("div.other-replies").css('background-color', '#98E23C');}, 1000);
+})(jQuery.noConflict(true));
