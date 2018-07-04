165794d836fa8f2555480fcebe15d137369e2dc8
Dmitry Akentyev <TogusaRusso@users.noreply.github.com>
2018-07-04 20:27:35 +0300

    Update outline.js
    Removed jQuery

--- outline.js
+++ outline.js
@@ -1,16 +1,13 @@
 // ==UserScript==
 // @name           Outline reply lines
-// @version        0.0.1
+// @version        0.0.2
 // @namespace      https://tweetdeck.twitter.com/
 // @auther         Togusa
 // @description    Outline reply lines
 // @include        https://web.tweetdeck.com/*
 // @include        https://tweetdeck.twitter.com/*
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @grant          none
 // ==/UserScript==
 
 
-(function($) {
-    setInterval(() => {$("div.other-replies").css('border', '2px dotted #98E23C');}, 1000);
-})(jQuery.noConflict(true));
+setInterval(() => document.querySelectorAll('.other-replies').forEach(el => {el.style.border = '2px dotted #98E23C'}), 1000);
